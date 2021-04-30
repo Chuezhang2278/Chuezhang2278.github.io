d4e96e9b90bcd8aa51c09e4b289ea40b85d76610
Chue Zhang <32043313+Chuezhang2278@users.noreply.github.com>
2021-04-30 17:29:29 -0400

    s

--- script.js
+++
@@ -1,8 +0,0 @@
-function copyToClipboard(element, attr) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).attr(attr)).select();
-    document.execCommand("copy");
-    $temp.remove();
-  }
-  
